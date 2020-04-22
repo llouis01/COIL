490f1e7961cbe9d5c69ce2119b806000b0527d29
llouis01 <loubenslouis@live.com>
2020-04-22 19:14:31 -0400

    WOrked on Mort_data
    1) imported income_data with the first row as column names instead of vector number.
    
    2) reshaped data set to long format using reshape(...direction = "long")
    
    3) removed the "id" column from inc_data; not relevant.
    
    4) ordered inc_data set by state and year.

--- Mortality_and_Budget/Mort_Data_Analysis.r
+++ Mortality_and_Budget/Mort_Data_Analysis.r
@@ -1,11 +1,6 @@
 ### Econ382, Final Project, Loubens Louis ###
 
-install.packages("readr")
-install.packages("stringr")
-library(readr)
-library(stringr)
-require(stringi)
-?stringi
+# Q1
 
 mort_data = mortality_data[(mortality_data$year >= 1993
                                  & mortality_data$year <= 2015),]
@@ -19,5 +14,11 @@ str_length(mort_data$age)
 str_sub(str_length(str_locate(mort_data$age, "-")[,1]))
 mort_data$age
 
+# Q7
 
-inc_data = reshape(income_data, varying = names(income_data)[2:24], timevar = "Year", sep = ".", direction = "long")
+inc_data = reshape(income_data, varying = names(income_data)[2:24], timevar = "year", sep = ".", direction = "long")
+
+
+inc_data = inc_data[order(inc_data$state, inc_data$year), ]
+
+inc_data = inc_data[ , -4]

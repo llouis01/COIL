68c3314904b072bfafda39177e8c3bd4f644e47e
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-09-16 21:54:28 -0400

    dataframe
    need to alphabetize the rows by animal

--- R_Lab/WM_ProblemSets/WM/502B_m4X.R
+++ R_Lab/WM_ProblemSets/WM/502B_m4X.R
@@ -5,3 +5,13 @@ x <- list('gnu', c(3:7), months = month.abb)
 x
 x$months[4]
 
+# make a dataframe and extract from it
+
+creatures = c("dog", "cat", "armadillo", "human")
+friendly = c(TRUE, TRUE, FALSE, TRUE)
+diet = c("cats", "mice", "termites", "twinkies(tm)")
+waking.hours = c(13.9, 11.5, 5.9, 16.0)
+
+creature.data = data.frame(row.names = creatures,
+                           friendly, diet, waking.hours)
+# will return

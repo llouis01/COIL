5ceed8c16530dace84d374df1745b348179870e9
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-09-16 23:23:59 -0400

    Built-in data sets
    working on the built-in cars data set and extracted the cars with even speed per hour

--- R_Lab/WM_ProblemSets/WM/502B_m4X.R
+++ R_Lab/WM_ProblemSets/WM/502B_m4X.R
@@ -1,4 +1,4 @@
-### 502B
+### 502B Sept 20th - Loubens Louis
 
 # create a list and extract value out of it
 x <- list('gnu', c(3:7), months = month.abb)
@@ -25,3 +25,15 @@ a = data.frame(z, y, p)
 a
 a_new = replace(a, a<0, 'NA')
 a_new
+
+
+# cars mean stopping distance
+cars
+mean(cars[, 2])
+
+cars = cars
+cars
+cars$speed
+cars$speed[cars$speed%%2==0]
+even_speed_cars = subset(cars, cars$speed%%2==0, select = c('speed', 'dist'))
+even_speed_cars

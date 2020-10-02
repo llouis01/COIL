450526a65a7038d2ce1d447bf53a54fe5772df97
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-10-01 23:59:11 -0400

    502A_m6X
    Create script and import library along w/ data

--- R_Lab/WM_ProblemSets/WM/502B_m6X.R
+++ R_Lab/WM_ProblemSets/WM/502B_m6X.R
@@ -1,2 +1,8 @@
 ######### Module 6, Oct. 4
 
+# import needed libraries
+library(xlsx)
+
+# 1a - create histogram of threepwood data
+te_data <- read.xlsx("C:\\Users\\Loube\\OneDrive - William & Mary\\Bus_Stats_BUAD502A\\Assignments\\buad502a-m6-expert-dataset-net-worth.xls", 1)
+

692d66b502ddd126079d9302cfb1ec309c21d55e
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-10-03 22:50:01 -0400

    t.test
    results were correct. p-value is .8 because meanns only have a difference of .45

--- R_Lab/WM_ProblemSets/WM/wine.R
+++ R_Lab/WM_ProblemSets/WM/wine.R
@@ -18,7 +18,7 @@ qqnorm(wine$Cellar.2)
 qqline(wine$Cellar.2, col = 'red')
 
 # 4c - sample means ofr age
-ybar_c1 <- mean(wine$Cellar.1, na.rm = T)
+ybar_c1 <- mean(wine$Cellar.1)
 ybar_c2 <- mean(wine$Cellar.2, na.rm = T)
 
 # 4d - differences in mean wine ages from cc's
@@ -34,7 +34,12 @@ sd_c1 <- sqrt(c1_var)
 sd_c2 <- sqrt(c2_var)
 
 # 4g
-se_cc <- sqrt(c1_var/length(wine$Cellar.1) + c2_var/length(na.omit(wine$Cellar.2)))
+c1 = wine$Cellar.1
+c1_n <- length(c1)
+c2 <- wine$Cellar.2
+c2_n <- length(na.omit(c2))
+
+se_cc <- sqrt(c1_var/c1_n + c2_var/c2_n)
 
 # 4h
 # H0: ybar_c1 = ybar_c2
@@ -45,11 +50,13 @@ tstat <- ybar_cc / se_cc
 
 # 5
 # H0: mu_c1 = mu_c2
-c1 = wine$Cellar.1
-c1_n <- length(c1)
-c2 <- wine$Cellar.2
-c2_n <- length(na.omit(c2))
-
 t.test(c1, c2, alternative = "two.sided")
 
 df_wines <- ((c1_var/c1_n + c2_var/c2_n)^2) / (((c1_var/c1_n)^2 / (c1_n - 1))  + ((c2_var/c2_n)^2 / (c2_n-1))) # verify df
+
+p_value <- 2 * pt(tstat, c1_n + c2_n - 2, lower = F)
+p_value2 <- 2* pt(tstat, min(c1_n - 1, c2_n - 2), lower = F)
+
+# the p-value which uses the lower degrees of freedom is more conser
+
+# At alpha 0.05, I fail to reject the null hypothesis stating that the mean ages in both cellars are equal #

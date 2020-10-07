3d307ae4a8d756b9f5ee82569387fdd36bb364c4
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-10-06 20:53:30 -0400

    LCG
    Constructed a linear confruential generator with mod 61 and seed 3

--- R_Lab/WM_ProblemSets/WM/502A_m7X.R
+++ R_Lab/WM_ProblemSets/WM/502A_m7X.R
@@ -17,3 +17,15 @@ casi_lm <- lm(casi$Casino.Revenue.in..1.000.000.s~casi$Number.of.free..5.poker.c
 casi_lm <- lm(casi$Casino.Revenue.in..1.000.000.s~casi$Number.of.free..5.poker.chips.given.away.)
 
 coef(casi_lm) # obtain coefficient
+
+
+#### Simulation Chapter 25
+# prime modulus multiplicative linear congruential generator
+
+# with a, m and x0
+
+x = numeric(61) # list must have length(mod) because generator is full period
+x[1] = 3 # set as seed
+for (i in 2:61) x[i] = (7 * x[i - 1]) %% 61
+print(x)
+# notice how the list went back to the seed

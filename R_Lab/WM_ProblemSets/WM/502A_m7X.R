ef2e4a7d9cf37ddbcc97296f9028c1a325de5c4b
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-10-06 21:16:52 -0400

    Plotting random numbers
    random numbers from lcg plot differently from runif generated numbers

--- R_Lab/WM_ProblemSets/WM/502A_m7X.R
+++ R_Lab/WM_ProblemSets/WM/502A_m7X.R
@@ -33,6 +33,8 @@ print(x)
 plot(x/61)
 plot(runif(14))
 
+
+par(mfrow = c(2,1))
 x = numeric(60) # list must have length(mod) because generator is full period
 x[1] = 3 # set as seed
 for (i in 2:60) x[i] = (7 * x[i - 1]) %% 61
@@ -42,6 +44,10 @@ for (i in 2:60) points(u[i - 1], u[i], pch = 10)
 points(u[60], u[1], pch = 10) # plot for pairs of random numbers
 
 
-u =
+u = runif(60)
+plot(0, 0, type = 'n', xlim = c(0,1), ylim = c(0.1))
+for (i in 2:60) points(u[i - 1], u[i], pch = 10)
+points(u[60], u[1], pch = 10)
+par(mfrow = c(1,1))
 
 

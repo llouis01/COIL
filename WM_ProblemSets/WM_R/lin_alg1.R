c6970c94ff04fea59ac2a9a50bc231b8e831f7b9
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-11-03 22:55:30 -0500

    Linear Algebra

--- WM_ProblemSets/WM_R/lin_alg1.R
+++ WM_ProblemSets/WM_R/lin_alg1.R
@@ -1,7 +1,18 @@
-row1 = c(1, 2, 1)
-row2 = c(0, 1, -4)
-row3 = c(-3, 0, 3)
-A = rbind(row1, row2, row3)
-rhs = c(0, 4, -6)
+# steps to solve linear equations
+# 2.1.29
+A = matrix(c(1, 2, -4, 2, 5, -6, -1, 3, 6), 3, 3, byrow = TRUE)
+A
+rhs = c(1, 0, -3)
 solve(A, rhs)
-# x1, x2, x3
+
+
+#### 2.1.30
+A = matrix(c(1, 3, -2, 3, 8, -4, -1, 4, -7), 3, 3, byrow=T)
+A
+rhs = c(-2, -6, 7)
+solve(A, rhs)
+
+### 2.2.2
+A = matrix(c(1, -2, 0, 1), 2, 2, byrow = T)
+rhs = c(6, -3)
+solve(A, rhs)

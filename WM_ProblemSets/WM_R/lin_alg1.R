a1766ca619a4a8e7f3bd1796ee8beddf2a706bed
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-11-04 00:27:58 -0500

    Closeout Alg

--- WM_ProblemSets/WM_R/lin_alg1.R
+++ WM_ProblemSets/WM_R/lin_alg1.R
@@ -16,3 +16,9 @@ solve(A, rhs)
 A = matrix(c(1, -2, 0, 1), 2, 2, byrow = T)
 rhs = c(6, -3)
 solve(A, rhs)
+
+## 2,2,3
+A = matrix(c(1, 4, -2, 0, 1, 3, 0, 0, 1), 3, 3, byrow=T)
+rhs = c(5, -3, 2)
+solve(A, rhs)
+A
